0f692c0065c5413d57f2e55c096471db96def9f4
DanielVertlib <danielvertlib@hotmail.ca>
2024-01-25 00:08:48 -0500

    Added resume and more skills

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import pdf from '../../assets/resume.pdf'
 
 import './about.scss'
 
@@ -20,19 +21,32 @@ const About = () => {
       <h2 className="about-role-text">A Frontend Developer.</h2>
       <span className="about-content-text">
         I am a Frontend Developer with a passion for creating beautiful, responsive, and functional web applications.
+        I love learning new tech and best practices while employing it in my work
+      </span>
+      <span className="about-content-text">
+        I have 5+ years of professional work experience building SaaS CMS platforms.
+        I am currently working on personal projects to further my skills and knowledge.
+      </span>
+      <span className="about-content-text">
+        Check out some of my projects below!
       </span>
       <div className="about-contact">
+        <a href={pdf} target="_blank" rel="noreferrer" className="resume-button">
+          <span type="button" className="resume-button-text">
+            Resume
+          </span>
+        </a>
         <a
           href={social.github}
           aria-label='github'
           className='link link-icon' >
-          <GitHubIcon />
+          <GitHubIcon fontSize="large" />
         </a>
         <a
           href={social.linkedIn}
           aria-label='linkedIn'
           className='link link-icon' >
-          <LinkedInIcon />
+          <LinkedInIcon fontSize="large" />
         </a>
       </div>
     </div>
--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -3,22 +3,27 @@ import React from 'react'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LaunchIcon from '@material-ui/icons/Launch'
 
+import spotifyist from '../../assets/spotifyist.png'
+import portfolio from '../../assets/portfolio.png'
+
 import './projects.scss'
 
 // todo: move projects cards to a separate file
 const projects = [
   {
     title: 'Spotifyist',
-    description: 'Leveraging the Spotify Web API, spotifyist allows users to explore their top artists and songs as well as their recently played songs.'
-    + 'Users can also create playlists and add songs to their existing playlists.',
+    description: 'Spotifyist allows users to explore their top artists and songs as well as their recently played songs.'
+    + ' Leveraging the Spotify Web API, users can also create spotify playlists from the user specific top charts.',
     stack: ['React', 'Redux', 'TypeScript', 'SCSS', 'HTML5'],
-    projectLink: 'https://spotifyist.netlify.app/'
+    projectLink: 'https://spotifyist.netlify.app/',
+    img: spotifyist
   },
   {
-    title: 'Project 2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    stack: ['React', 'Redux', 'React-Router', 'TypeScript', ],
-    projectLink: 'https://google.com'
+    title: 'Portfolio Website',
+    description: 'Portfolio Site built with React and SCSS.',
+    stack: ['React', 'SCSS', 'Javascript', 'HTML5', 'Material-UI'],
+    projectLink: 'https://google.com',
+    img: portfolio
   }
 ]
 
@@ -27,37 +32,42 @@ const social = {
   github: 'https://github.com/DanielVertlib'
 }
 
-const ProjectCard = ({ title, description, stack, projectLink }) => {
+const ProjectCard = ({ title, description, stack, projectLink, img }) => {
   return (
     <div className="project-card">
-      <div className="project-card-title">
-        <h3>{title}</h3>
+      <div className="project-card-img">
+        <img src={img} alt="spotifyist" />
       </div>
       <div className="project-card-content">
-        <p>{description}</p>
-      </div>
-      {/* <div className="project-card-tech"> */}
-        <ul className="project-card-tech">
-          {stack.map((item, index) =>
-            <li className="project-tech-item" key={index}>
-              {item}
-            </li>)
-          }
-        </ul>
-      {/* </div> */}
-      <div className="project-card-links">
-        <a
-          href={social.github}
-          aria-label='github'
-          className='link link-icon' >
-          <GitHubIcon />
-        </a>
-        <a
-          href={projectLink}
-          aria-label='launch'
-          className='link link-icon' >
-          <LaunchIcon />
-        </a>
+        <div className="project-card-title">
+          <h3>{title}</h3>
+          <div className="project-card-links">
+            <a
+              href={social.github}
+              aria-label='github'
+              className='link link-icon' >
+              <GitHubIcon />
+            </a>
+            <a
+              href={projectLink}
+              aria-label='launch'
+              className='link link-icon' >
+              <LaunchIcon />
+            </a>
+          </div>
+        </div>
+        <div className="project-card-description">
+          <p>{description}</p>
+        </div>
+        {/* <div className="project-card-tech"> */}
+          <ul className="project-card-tech">
+            {stack.map((item, index) =>
+              <li className="project-tech-item" key={index}>
+                {item}
+              </li>)
+            }
+          </ul>
+        {/* </div> */}
       </div>
     </div>
   )
